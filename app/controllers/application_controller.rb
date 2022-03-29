07cd96e168c2d79ce79f40bda9ae2a013a6f7a3e
megumi04 <m09067717957@docomo.ne.jp>
2022-03-29 20:33:40 +0900

    ユーザー管理機能の実施

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :sign_up, keys: [:nickname, :birthday, :first_name, :last_name, :first_name_kana, :last_name_kana]
+    )
+  end
 
   private
   def basic_auth

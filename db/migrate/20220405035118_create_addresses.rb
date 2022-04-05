1c24a3504e1d94b732ab7044eec0e40d8f5c5d3b
megumi04 <m09067717957@docomo.ne.jp>
2022-04-05 17:15:30 +0900

    マイグレーションエラーの為

--- db/migrate/20220405035118_create_addresses.rb
+++ db/migrate/20220405035118_create_addresses.rb
@@ -1,7 +1,7 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-      t.string     :postal_code,    null: false
+      t.string     :post_code,    null: false
       t.integer    :state_id,       null: false
       t.string     :city,           null: false
       t.string     :house_number,   null: false
